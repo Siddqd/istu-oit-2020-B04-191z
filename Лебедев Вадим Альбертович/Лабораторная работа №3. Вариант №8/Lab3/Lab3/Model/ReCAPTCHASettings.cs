5d395d4131fa9b107c206f97be8b1c59fe370f52
LebedevVadim <65890518+LebedevVadim@users.noreply.github.com>
2020-05-25 12:38:32 +0400

    Лаба 3
    Сделал на asp. net core. Капча - google

---
+++ Лебедев Вадим Альбертович/Лабораторная работа №3. Вариант №8/Lab3/Lab3/Model/ReCAPTCHASettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Model
+{
+    public class ReCAPTCHASettings
+    {
+        public string ReCAPTCHA_Site_Key { get; set; }
+        public string ReCAPTCHA_Secret_key { get; set; }
+    }
+}
