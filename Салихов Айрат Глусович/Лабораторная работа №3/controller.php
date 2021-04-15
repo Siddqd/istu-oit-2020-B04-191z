6f6f83c7449c01de67012647728a016f1b6e902c
airatchaplin <64369948+airatchaplin@users.noreply.github.com>
2021-04-15 13:45:04 +0400

    Update controller.php

--- Салихов Айрат Глусович/Лабораторная работа №3/controller.php
+++ Салихов Айрат Глусович/Лабораторная работа №3/controller.php
@@ -2,6 +2,7 @@
   
   function getAllById($id){
       $db = new SQLite3("site.db");
+    $id = SQLite3::escapeString($id);
       $sql = "SELECT * FROM site WHERE id='$id'";
       $sqlCount = "SELECT COUNT(id) AS count FROM site";
       $result1 = $db->query($sqlCount);
@@ -12,6 +13,11 @@
   }
   
   function addTable($heading,$text,$img){
+    
+       $heading = SQLite3::escapeString($headingd);
+       $text = SQLite3::escapeString($text);
+       $img = SQLite3::escapeString($img);
+
       $db = new SQLite3("site.db");
       $sqlInsert = "INSERT INTO site (heading,text,img) VALUES ('$heading' ,'$text', '$img')";
       $db->query($sqlInsert);
